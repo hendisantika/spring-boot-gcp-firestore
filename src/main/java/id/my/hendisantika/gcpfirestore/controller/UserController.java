f67d8d723735080e149690d5806dcdd82b0dd7b0
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 05:16:34 +0700

    feat: Add findAll in UserController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/gcpfirestore/controller/UserController.java
+++ src/main/java/id/my/hendisantika/gcpfirestore/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -55,4 +57,10 @@ public class UserController {
                         .ok(user1))).switchIfEmpty(Mono
                         .just(ResponseEntity.notFound().build()));
     }
+
+    @GetMapping
+    private Flux<User> findAll() {
+        return userService.findAll();
+    }
+
 }
