682b2eaf8c614450074ada6c86f115723ed6d930
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 05:12:30 +0700

    feat: Add UserService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpfirestore/service/UserService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.gcpfirestore.service;
+
+import id.my.hendisantika.gcpfirestore.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-gcp-firestore
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/25
+ * Time: 05.11
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+}
