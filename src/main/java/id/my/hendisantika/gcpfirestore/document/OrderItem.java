456cc0f78e6fd7537181be57e1dac5e5cf6ffe36
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 05:28:16 +0700

    feat: Add OrderItem Document🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpfirestore/document/OrderItem.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.gcpfirestore.document;
+
+import com.google.cloud.spring.data.firestore.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-gcp-firestore
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/25
+ * Time: 05.27
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collectionName = "order_items")
+class OrderItem {
+    private String description;
+    private Long quantity;
+}
