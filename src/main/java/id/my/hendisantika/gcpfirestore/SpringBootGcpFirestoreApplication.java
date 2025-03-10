1066ea65e6955e2667bbc95e760c4ab6d362d14e
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 05:06:47 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpfirestore/SpringBootGcpFirestoreApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.gcpfirestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpFirestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGcpFirestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/gcpfirestore/SpringBootGcpFirestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.gcpfirestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGcpFirestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
